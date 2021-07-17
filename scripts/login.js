2e3e270b489456974cdd8b3794c5304cb23e4392
Chris Hundhausen <hundhaus@wsu.edu>
2021-07-16 17:03:29 -0700

    Added Login Page validation * Error messages show at top of form * Error messages link to corresponding fields * Still need to document code and do final testing

---
+++ scripts/login.js
@@ -0,0 +1,43 @@
+const loginBtn = document.getElementById("loginBtn");
+const emailField = document.getElementById("email");
+const passwordField = document.getElementById("password");
+const errBox = document.getElementById("errorBox");
+const emailErr = document.getElementById("emailError");
+const passwordErr = document.getElementById("passwordError");
+
+loginBtn.addEventListener("click",function(e) {
+   e.preventDefault();
+   let emailValid = !emailField.validity.typeMismatch && !emailField.validity.valueMissing;
+   let passwordValid = !passwordField.validity.patternMismatch && !passwordField.validity.valueMissing;
+   if (emailValid && passwordValid) {
+      errBox.classList.add("hidden");
+      emailErr.classList.add("hidden");
+      passwordErr.classList.add("hidden");
+      return;
+   }
+   errBox.classList.remove("hidden");
+   if (!emailValid) {
+       emailErr.classList.remove("hidden");
+       emailErr.focus();
+   } else {
+       emailErr.classList.add("hidden");
+   }
+   if (!passwordValid) {
+       passwordErr.classList.remove("hidden");
+       if (emailValid) {
+            passwordErr.focus();
+       }
+   } else {
+       passwordErr.classList.add("hidden");
+   }
+});
+
+emailField.addEventListener("keypress",function(e) {
+    if (e.code === "Enter")
+        loginBtn.click();
+});
+
+passwordField.addEventListener("keypress",function(e) {
+    if (e.code === "Enter")
+     loginBtn.click();
+});
